59f822065b9d137536814b6f5c880e3508c71900
Ziv Peleg <zivpeleg@gmail.com>
2021-01-31 00:49:56 +0200

    Added some buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function GuessButton(props) {
+  console.log(props.options);
+
+  const [choice, setChoice] = useState(0);
+  function changeChoice() {
+    let newChoice = choice + 1;
+    if (newChoice == props.options.length) {
+      newChoice = 0;
+    }
+    setChoice(newChoice)
+  }
+  return <button disabled={props.disabled} onClick={() => changeChoice()}>{props.options[choice]}</button>;
+}
+
+function SubmitButton(props){
+  return <button onClick={()=>props.onClick()}>Submit</button>
+}
+
+function ChoiceRow(props) {
+  console.log(props.options);
+  return (
+    <div>
+      {(Array(props.guessCount).fill().map((i) => <GuessButton disabled={false} key={i} options={props.options} />))}
+      <SubmitButton />
+    </div>
+  );
+}
 
 function App() {
+  const options = [1, 2, 3, 4, "a"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChoiceRow guessCount={4} options={options} />
     </div>
   );
 }
