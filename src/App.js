855ad1d906bd6dd17b98c68b7405f9ddd4be7148
Ziv Peleg <zivpeleg@gmail.com>
2021-02-19 16:19:38 +0200

    Move more state to App

--- src/App.js
+++ src/App.js
@@ -14,33 +14,29 @@ function range(size, startAt = 0) {
   return [...Array(size).keys()].map(i => i + startAt);
 }
 
-
 function ChoiceRow(props) {
-  const [choices, setChoices] = useState(Array(props.guessCount).fill(0))
-  console.log(choices)
-  function changeChoice(idx) {
-    let c = choices[idx] + 1
-    if (c === props.options.length) {
-      c = 0
-    }
-    let newChoices = choices.slice()
-    newChoices[idx] = c
-    setChoices(newChoices)
-  }
-  function submitChoices() {
-    props.submitGuess(choices)
-  }
   return (
     <div>
       {(range(props.guessCount).map((i) =>
         <GuessButton
           enabled={props.enabled}
           key={i}
-          choice={props.options[choices[i]]}
-          onClick={() => changeChoice(i)} />))}
-      {props.enabled ? <SubmitButton onClick={() => submitChoices()} /> : (null)}
+          choice={props.choices[i]}
+          onClick={() => props.changeChoice(i)} />))}
+      {props.enabled ? <SubmitButton onClick={() => props.submit()} /> : (null)}
     </div>
   );
+}
+
+function GuessMark(props) {
+  switch (props.success) {
+    case "exact":
+      return <p>X</p>
+    case "correct":
+      return <p>O</p>
+    case "incorrect":
+      return <p>.</p>
+  }
 }
 
 function PastGuess(props) {
@@ -55,22 +51,49 @@ function GuessContainer(props) {
   return (
     <div>
       {props.guessHistory.map((g) => <PastGuess guess={g} options={props.options} />)}
+      {/* {props.guessHistory.map((g) => <p>{g}</p>)} */}
     </div>
   )
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function App() {
-  const options = [1, 2, 3, 4, "a"];
+  const GUESS_COUNT = 4
+  const options = ["b", 2, 3, 4, "a"];
+
+  // Not sure this is the correct way to do this, but it's a cool hack
+  const [theGuess, _] = useState(Array(GUESS_COUNT).fill().map(() => getRandomInt(options.length)))
+
   const [guessHistory, setGuessHistory] = useState([])
-  function showGuess(guess) {
-    const newGuessHistory = guessHistory.slice().concat([guess])
+  function showGuess() {
+    const newGuessHistory = guessHistory.slice().concat([choices.slice()])
     setGuessHistory(newGuessHistory)
-    console.log(guess)
   }
+
+  const [choices, setChoices] = useState(Array(GUESS_COUNT).fill(0))
+  function changeChoice(idx) {
+    let c = choices[idx] + 1
+    if (c === options.length) {
+      c = 0
+    }
+    let newChoices = choices.slice()
+    newChoices[idx] = c
+    setChoices(newChoices)
+  }
+
   return (
     <div className="App">
       <GuessContainer guessHistory={guessHistory} options={options} />
-      <ChoiceRow guessCount={4} enabled={true} options={options} submitGuess={(x) => showGuess(x)} />
+      <ChoiceRow
+        guessCount={GUESS_COUNT}
+        enabled={true}
+        options={options}
+        changeChoice={changeChoice}
+        submit={() => showGuess()}
+        choices={choices.map((i)=>options[i])} />
     </div>
   );
 }
